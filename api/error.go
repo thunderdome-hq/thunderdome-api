61897561ae26a55de3caab8f4cb2a3814780eb93
ludwighultqvist1 <ludwig.hultqvist@gmail.com>
2023-02-17 11:26:57 +0100

    fix: type error in CLI

--- api/error.go
+++ api/error.go
@@ -7,22 +7,20 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-type Type string
-
 const (
-	AuthError   Type = "authentication"
-	StatusError Type = "status"
-	JoinError   Type = "join"
-	LeaveError  Type = "leave"
-	AcceptError Type = "accept"
-	VerifyError Type = "verify"
-	ListError   Type = "list"
-	ClaimError  Type = "claim"
-	DropError   Type = "drop"
-	CLIError    Type = "CLI"
+	AuthError   = "authentication"
+	StatusError = "status"
+	JoinError   = "join"
+	LeaveError  = "leave"
+	AcceptError = "accept"
+	VerifyError = "verify"
+	ListError   = "list"
+	ClaimError  = "claim"
+	DropError   = "drop"
+	CLIError    = "CLI"
 )
 
-func Error(code codes.Code, errorType Type, cause string, args ...interface{}) error {
+func Error(code codes.Code, error string, cause string, args ...interface{}) error {
 	text := fmt.Sprintf(cause, args)
-	return status.Errorf(code, fmt.Sprintf("%s failed –– %s.", errorType, text))
+	return status.Errorf(code, fmt.Sprintf("%s failed –– %s.", error, text))
 }
