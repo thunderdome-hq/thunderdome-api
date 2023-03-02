331be82ce5fcd19d5ab2967d20a6df8d80418b3d
weslien <gustav.weslien@gmail.com>
2023-03-02 14:20:22 +0100

    🤘

--- api/thunderdome.pb.gw.go
+++ api/thunderdome.pb.gw.go
@@ -195,7 +195,7 @@ func RegisterThunderdomeHandlerServer(ctx context.Context, mux *runtime.ServeMux
 
 	})
 
-	mux.Handle("GET", pattern_Thunderdome_GithubWebhook_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("POST", pattern_Thunderdome_GithubWebhook_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
@@ -305,7 +305,7 @@ func RegisterThunderdomeHandlerClient(ctx context.Context, mux *runtime.ServeMux
 
 	})
 
-	mux.Handle("GET", pattern_Thunderdome_GithubWebhook_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("POST", pattern_Thunderdome_GithubWebhook_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
