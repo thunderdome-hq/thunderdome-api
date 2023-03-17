353ed9959be27cbb1dabec698306e99d5591a0f0
weslien <gustav.weslien@gmail.com>
2023-03-17 10:58:41 +0100

    WIP

--- api/error.go
+++ api/error.go
@@ -9,19 +9,6 @@ import (
 
 type Type string
 
-const (
-	AuthError   Type = "authentication"
-	StatusError Type = "status"
-	JoinError   Type = "join"
-	LeaveError  Type = "leave"
-	AcceptError Type = "accept"
-	VerifyError Type = "verify"
-	ListError   Type = "list"
-	ClaimError  Type = "claim"
-	DropError   Type = "drop"
-	CLIError    Type = "CLI"
-)
-
 func Error(code codes.Code, errorType Type, cause string, args ...interface{}) error {
 	text := fmt.Sprintf(cause, args...)
 	return status.Errorf(code, fmt.Sprintf("%s failed –– %s.", errorType, text))
